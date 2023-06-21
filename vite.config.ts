a6474272d3711db8f4a6d52f6e140d1385707a72
Filip Östermark <filip.ostermark@gmail.com>
2023-06-21 14:28:00 +0200

    New logo

--- vite.config.ts
+++ vite.config.ts
@@ -2,49 +2,57 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
+const pwaPlugin = VitePWA({
+  registerType: 'prompt',
+  // add this to cache all the imports
+  workbox: {
+    globPatterns: ["**/*"],
+  },
+  // add this to cache all the
+  // static assets in the public folder
+  includeAssets: [
+    "**/*",
+  ],
+  manifest: {
+    name: "Ukodus",
+    short_name: "Ukodus",
+    description: "A simple sudoku app",
+    icons: [
+      {
+        src: "/logo128.png",
+        sizes: "128x128",
+        type: "image/png",
+        purpose: "any"
+      },
+      {
+        src: "/logo256.png",
+        sizes: "256x256",
+        type: "image/png",
+        purpose: "any"
+      },
+      {
+        src: "/logo512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable"
+      }
+    ],
+    display: "standalone",
+    scope: "/Reactukodus",
+    start_url: "/Reactukodus",
+    theme_color: "#171717",
+    background_color: "#000000",
+    orientation: "portrait"
+  },
+  devOptions: {
+    enabled: true
+  }
+})
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/reactukodus",
+  base: "/Reactukodus",
   plugins: [
-    react(),
-    VitePWA({
-      registerType: 'prompt',
-      // add this to cache all the imports
-      workbox: {
-        globPatterns: ["**/*"],
-      },
-      // add this to cache all the
-      // static assets in the public folder
-      includeAssets: [
-        "**/*",
-      ],
-      manifest: {
-        name: "Ukodus",
-        short_name: "Ukodus",
-        description: "A simple sudoku app",
-        icons: [
-          {
-            src: "/vite.svg",
-            sizes: "512x512",
-            type: "image/svg+xml"
-          },
-          {
-            src: "/37499.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable"
-          }
-        ],
-        display: "standalone",
-        scope: "/",
-        start_url: "/",
-        theme_color: "#171717",
-        background_color: "#000000",
-        orientation: "portrait"
-      },
-      devOptions: {
-        enabled: true
-      }
-    })
+    react(), pwaPlugin
   ]
 })
