0c3e426fea158a71deda09dbbfb057a20b7dc846
Filip Östermark <filip.ostermark@gmail.com>
2023-06-21 13:51:02 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/reactukodus/sw.js', { scope: '/reactukodus/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
